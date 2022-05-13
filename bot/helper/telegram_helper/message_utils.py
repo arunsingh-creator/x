9120f7636f63a12f13a64a22e5ba672783caccfd
ARUN NOOB <83957038+arunsingh-creator@users.noreply.github.com>
2022-05-13 13:20:01 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -6,7 +6,7 @@ from pyrogram.errors import FloodWait
 from pyrogram import enums
 
 from bot import AUTO_DELETE_MESSAGE_DURATION, LOGGER, status_reply_dict, status_reply_dict_lock, \
-                Interval, DOWNLOAD_STATUS_UPDATE_INTERVAL, RSS_CHAT_ID, rss_session, bot
+                Interval, DOWNLOAD_STATUS_UPDATE_INTERVAL, bot
 from bot.helper.ext_utils.bot_utils import get_readable_message, setInterval
 
 
